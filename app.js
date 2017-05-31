dd45164c743f8f3c82377c1c47a274cf2e5094a5
haozhaohang <745184644@qq.com>
2017-05-31 20:07:49 +0800

    feat: update library

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // 加载express模块
 var express = require( 'express' );
 // 加载模板处理模块
-var swig = require( 'swig' );
+// var swig = require( 'swig' );
 // 加载数据库模块
 var mongoose = require( 'mongoose' );
 // 加载bodyparse模块,用来处理post提交过来的数据
@@ -25,15 +25,15 @@ app.use( '/public', express.static( __dirname + '/public' ) );
 // 配置应用模板
 // 定义当前应用所使用的模板引擎
 // 第一个参数:模板引擎名称，同时也是模板文件的后缀；第二个参数表示用于解析处理模板内容的方法
-app.engine( 'html', swig.renderFile );
+// app.engine( 'html', swig.renderFile );
 // 设置模板文件存放目录，第一个参数必须是views;第二个参数是目录地址
-app.set( 'views', './public' );
+// app.set( 'views', './public' );
 // 注册所使用的模板引擎,第一个参数必须是view engine,第二个参数和app.engine这个方法中定义的模板引擎的名称(第一个参数)是一致的
-app.set( 'view engine', 'html' );
+// app.set( 'view engine', 'html' );
 // 在开发过程中，需要取消模板引擎的缓存
-swig.setDefaults({
-	cache: false
-});
+// swig.setDefaults({
+// 	cache: false
+// });
 
 /**
  * 首页
