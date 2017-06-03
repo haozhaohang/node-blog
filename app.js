599cd49b32ea2d063554123aca369e6c472edf6d
haozhaohang <745184644@qq.com>
2017-06-03 12:14:18 +0800

    fix: fix bug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // 加载express模块
 var express = require( 'express' );
 // 加载模板处理模块
-// var swig = require( 'swig' );
+var swig = require( 'swig' );
 // 加载数据库模块
 var mongoose = require( 'mongoose' );
 // 加载bodyparse模块,用来处理post提交过来的数据
@@ -25,15 +25,15 @@ app.use( '/public', express.static( __dirname + '/public' ) );
 // 配置应用模板
 // 定义当前应用所使用的模板引擎
 // 第一个参数:模板引擎名称，同时也是模板文件的后缀；第二个参数表示用于解析处理模板内容的方法
-// app.engine( 'html', swig.renderFile );
+app.engine( 'html', swig.renderFile );
 // 设置模板文件存放目录，第一个参数必须是views;第二个参数是目录地址
-// app.set( 'views', './public' );
+app.set( 'views', './public' );
 // 注册所使用的模板引擎,第一个参数必须是view engine,第二个参数和app.engine这个方法中定义的模板引擎的名称(第一个参数)是一致的
-// app.set( 'view engine', 'html' );
+app.set( 'view engine', 'html' );
 // 在开发过程中，需要取消模板引擎的缓存
-// swig.setDefaults({
-// 	cache: false
-// });
+swig.setDefaults({
+    cache: false
+});
 
 /**
  * 首页
@@ -41,7 +41,7 @@ app.use( '/public', express.static( __dirname + '/public' ) );
  * res responent
 */
 // app.get( '/', function( req, res, next ) {
-// 	res.send( '<h1>demo</h1>' );
+//  res.send( '<h1>demo</h1>' );
 // });
 
 /**
@@ -49,7 +49,7 @@ app.use( '/public', express.static( __dirname + '/public' ) );
  * 第一个参数: 表示模板的文件，相对于views目录 views/index.html
  * 第二个参数: 传递给模板使用的数据
 */
-// 	res.render( 'index' );
+//  res.render( 'index' );
 // } )
 
 // bodyparser设置
@@ -84,12 +84,12 @@ app.use( '/', require( './routers/main' ) );
 
 // 监听http请求
 mongoose.connect( 'mongodb://localhost:27018/blog', function( err ) {
-	if ( err ) {
-		console.log( '数据库连接失败' );
-	} else {
-		console.log( '数据库连接成功' );
-		app.listen( 8888 );
-	}
+    if ( err ) {
+        console.log( '数据库连接失败' );
+    } else {
+        console.log( '数据库连接成功' );
+        app.listen( 8888 );
+    }
 } );
 
 console.log( 'listen 8888' );
