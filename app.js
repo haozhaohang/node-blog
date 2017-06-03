42c75ca942cfdae7ab16a628b583ee506b7f967b
haozhaohang <745184644@qq.com>
2017-06-03 16:12:20 +0800

    update code

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var Cookies = require( 'cookies' );
 // 创建app应用 => nodejs http.createServer();
 var app = express();
 
-var User = require( './models/User' );
-
 // 设置静态文件托管
 // 当用户访问的是以/public开始，那么直接返回对应__dirname + '/public'下的文件
 app.use( '/public', express.static( __dirname + '/public' ) );
@@ -35,46 +33,8 @@ swig.setDefaults({
     cache: false
 });
 
-/**
- * 首页
- * req request
- * res responent
-*/
-// app.get( '/', function( req, res, next ) {
-//  res.send( '<h1>demo</h1>' );
-// });
-
-/**
- * 读取views目录下的指定文件，解析并返回客户端
- * 第一个参数: 表示模板的文件，相对于views目录 views/index.html
- * 第二个参数: 传递给模板使用的数据
-*/
-//  res.render( 'index' );
-// } )
-
 // bodyparser设置
 app.use( bodyParser.urlencoded({ extended: true }) );
-
-// 设置cookies
-app.use( function( req, res, next ) {
-    req.cookies = new Cookies( req, res );
-    req.userInfo = {};
-    // 解析cookies信息
-    if ( req.cookies.get( 'userInfo' ) ) {
-        try {
-            req.userInfo = JSON.parse( req.cookies.get( 'userInfo' ) );
-            // 获取当前登录用户是否是管理员
-            User.findById( req.userInfo._id ).then( function( userInfo ) {
-                req.userInfo.isAdmin = Boolean( userInfo.isAdmin );
-                next();
-            } );
-        } catch(e) {
-            next();
-        }
-    } else {
-        next();
-    }
-} );
 
 /**
  * 根据不同的功能划分模块
